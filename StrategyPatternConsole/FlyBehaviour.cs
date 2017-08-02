727c5cc7d7310e61c69c70b93190de7131a34f6b
kchungsemchong <kchungsemchong@gmail.com>
2017-08-02 22:06:23 +0400

    Initial Commit

---
+++ StrategyPatternConsole/FlyBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPatternConsole
+{
+    public class FlyBehaviour : IMovementBehaviour
+    {
+	   public void Move()
+	   {
+		  Console.WriteLine("I can fly!");
+	   }
+    }
+}
